a5e51e3903df16e49ddaab12b07d30a0d7cbc1c3
Osama Rashid <30891941+osama996@users.noreply.github.com>
2019-06-07 23:35:41 +0200

    :checked
    added :checked to input, as when calling the API before the change the todos with a true completed value wasn't checked.
    *I hope this is the right way as I'm new to vue.

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
     <p>
-      <input type="checkbox" v-on:change="markComplete">
+      <input type="checkbox" v-on:change="markComplete" v-bind:checked="todo.completed">
       {{todo.title}}
       <button @click="$emit('del-todo', todo.id)" class="del">x</button>
       </p>
